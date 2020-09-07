b434a3147d503bccaa5fa0f0d95f31434fee21ac
Srinath Vasudevan <Loadingdotdotdot>
2020-09-06 23:30:24 -0400

    Added a projects page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('.mainheader .header').height($(window).height());
 	$('.aboutmeheader').height($(window).height()/2);
+	$('.projectheader .header').height($(window).height()/2);
 })
 
